48411b9ad06a0c297dfae5e5580530493eccf875
azykdev <azyk9114@gmail.com>
2024-02-25 18:54:58 +0500

    #1 - Create project

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="inputDiv">
+    <input type="number" v-model="getNum">
+    <h1>{{ getNum }}</h1>
+  </div>
+  <div style="display: flex; align-items: center; justify-content: center; flex-wrap: wrap;">
+    <TrafficLight v-for="n in getNum" :randomNum="randomNum()" />
+  </div>
+</template>
+
+<script>
+import TrafficLight from './components/TrafficLight.vue';
+
+export default {
+  name: 'App',
+  components: { TrafficLight },
+  data() {
+    return {
+      getNum: 0
+    }
+  },
+  methods: {
+    randomNum() {
+      return Math.floor(Math.random() * 10)
+    }
+  },
+}
+</script>
+
+<style scoped>
+.inputDiv {
+  width: 30%;
+  margin: 20px auto;
+  text-align: center;
+}
+
+input {
+  width: 100%;
+  padding: 10px;
+}
+</style>
---
+++ src/components/TrafficLight.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="svetofor">
+    <div class="chiroq" :class="[second > 0 && second < 5 ? 'red' : '']"></div>
+    <div class="chiroq" :class="[(second > 5 && second < 8) || (second > 12 && second < 15) ? 'yellow' : '']"></div>
+    <div class="chiroq" :class="[second > 8 && second < 12 ? 'green' : '']"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      second: 0,
+    }
+  },
+  props: {
+    randomNum: Number
+  },
+  methods: {
+    secundomer() {
+      setInterval(() => {
+        this.second++
+        if (this.second == 15) this.second = 0
+      }, 1000)
+    },
+    timer() {
+      setTimeout(() => {
+        console.log(this.mlSecond);
+        this.secundomer()
+      }, this.mlSecond)
+    }
+  },
+  computed: {
+    mlSecond() {
+      return this.randomNum * 1000
+    }
+  },
+  mounted() {
+    this.timer()
+  }
+}
+</script>
+
+<style scoped>
+.svetofor {
+  background: #000;
+  display: inline-block;
+  padding: 20px;
+  margin: 5px;
+  border-radius: 10px;
+
+}
+
+.chiroq {
+  width: 100px;
+  height: 100px;
+  background: #333;
+  border-radius: 50%;
+  margin-bottom: 10px;
+}
+
+.red {
+  background: red;
+}
+
+.yellow {
+  background: yellow;
+}
+
+.green {
+  background: green;
+}
+</style>
